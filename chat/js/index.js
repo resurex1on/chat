c182d97d8cdab05f7686743f037c312a64923de7
Dushez <vladleonchik@mail.ru>
2019-09-04 03:33:56 +0300

    added package.json and apdated some files

--- chat/js/index.js
+++ chat/js/index.js
@@ -1,12 +1,7 @@
-var http = require('http');
-var fs = require('fs')
+var  express = require('express')
+var app = express()
 
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    let path = __dirname
-    path = path.substring(0, path.length-3)
-    var myReadStream = fs.createReadStream(path + '/index.html', 'utf8');
-    myReadStream.pipe(response);
-  });
-  
-  server.listen(3000, '127.0.0.1');
+let path = __dirname
+path = path.substring(0,path.length-2)
+app.use('/', express.static(path + '/'))
+app.listen(8000)
